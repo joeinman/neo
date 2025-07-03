ef27000ed708b7245e888939121daf207f37df0e
joeinman <joe.inman8@gmail.co.uk>
2025-07-03 22:33:18 +0100

    Refactor Rectangle class: inline render method and remove rectangle.cpp file

--- include/neo/component/rectangle.hpp
+++ include/neo/component/rectangle.hpp
@@ -27,7 +27,16 @@ public:
         properties_.set<Color>("color", color);
     }
 
-    std::pair<Position, std::vector<std::vector<Color>>> render() override;
+    std::pair<Position, std::vector<std::vector<Color>>> render() override
+    {
+        int   x      = properties_.get<int>("x").value_or(0);
+        int   y      = properties_.get<int>("y").value_or(0);
+        int   width  = properties_.get<int>("width").value_or(1);
+        int   height = properties_.get<int>("height").value_or(1);
+        Color color  = properties_.get<Color>("color").value_or(Color(255, 255, 255));
+
+        return {{x, y}, std::vector<std::vector<Color>>(height, std::vector<Color>(width, color))};
+    }
 };
 
 }  // namespace jsi::neo
